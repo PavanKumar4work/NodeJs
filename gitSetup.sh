41eafbaef95d6949dc9522583ce4f711180d4d42
PavanKumar <pavankumar4work@gmail.com>
2017-12-21 23:14:49 +0530

    Install Steps

---
+++ gitSetup.sh
@@ -0,0 +1,5 @@
+PS F:\NODEJS> git config --global user.email "pavankumar4work@gmail.com"
+PS F:\NODEJS> git config --global user.name "PavanKumar"
+
+PS F:\NODEJS> git remote add origin https://github.com/PavanKumar4work/NodeJs.git
+PS F:\NODEJS> git push -u origin master
